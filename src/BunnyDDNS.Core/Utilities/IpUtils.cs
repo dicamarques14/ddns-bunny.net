94ee52846daf8aa835951fe33cb3c04b722dcdf0
Dg <dicamarques14@users.noreply.github.com>
2026-02-24 15:31:03 +0000

    Update IpUtils.cs
    Ipv4 v6 switch

--- src/BunnyDDNS.Core/Utilities/IpUtils.cs
+++ src/BunnyDDNS.Core/Utilities/IpUtils.cs
@@ -3,19 +3,32 @@ using System.Net.Mime;
 
 namespace BunnyDDNS.Core.Utilities;
 
+public enum IpVersion
+{
+    IPv4,
+    IPv6
+}
+
 public class IpUtils
 {
     private static HttpClient Client;
+
     public IpUtils(HttpClient client)
     {
-        // Setup HttpClient
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
         client.DefaultRequestHeaders.UserAgent.Add(Meta.UserAgent);
         Client = client;
     }
-    public string GetCurrentIp()
+
+    public string GetCurrentIp(IpVersion ipVersion = IpVersion.IPv4)
     {
-        var url = "https://text.myip.wtf";
+        var url = ipVersion switch
+        {
+            IpVersion.IPv4 => "https://ipv4.text.myip.wtf",
+            IpVersion.IPv6 => "https://ipv6.text.myip.wtf",
+            _ => throw new ArgumentOutOfRangeException(nameof(ipVersion), "Unsupported IP version.")
+        };
+
         var response = Client.GetAsync(url).Result;
         response.EnsureSuccessStatusCode();
         return response.Content.ReadAsStringAsync().Result.Trim(Environment.NewLine.ToCharArray());
